f8a2dddec0a6b5baaa664b7aa6a4e6ed630bcb7f
kellandrosu <kellandr@oregonstate.edu>
2018-05-24 16:16:28 -0700

    initial commit

---
+++ RedditViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditViewer
+//
+//  Created by Andrius Kelly on 5/24/18.
+//  Copyright © 2018 Andrius Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
